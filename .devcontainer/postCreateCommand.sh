8f5675d3d943bbd913c781aa3989cb408b686a7b
Kenichi Sato <310dev@gmail.com>
2026-02-18 18:28:27 +0000

    miseの導入

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
-set -eux  # -e: エラーで停止, -u: 未定義変数禁止, -x: コマンド出力
+set -ex  # -e: エラーで停止, -x: コマンド出力
 
 # 所有権変更
 sudo mkdir -p /workspace/.venv
 sudo chown -R vscode:vscode /workspace/.venv || true
-sudo mkdir -p /home/vscode/.cache/uv
-sudo chown -R vscode:vscode /home/vscode/.cache/uv || true
 
-# Pythonのインストール
-uv python install ${PYTHON_VERSION}
+# 実行環境のインストール
+curl https://mise.run | sh
+mise trust --all
+mise install
+echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.bashrc
+source ~/.bashrc
 
 # 仮想環境を作成
 if [ ! -d ".venv" ]; then
